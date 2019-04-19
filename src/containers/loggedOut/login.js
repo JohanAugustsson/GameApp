ddfb9015e318667af5eee3230dbba8f5d8cd4801
JohanAugustsson <j.augustsson80@gmail.com>
2019-04-19 18:42:35 +0200

    create navigotor

---
+++ src/containers/loggedOut/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen Funkar</Text>
+      </View>
+    );
+  }
+}
+
+
+export default (LoginScreen);
